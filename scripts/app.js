3bffa6130366119e5d8c7a0586b0b394be47bbc1
heathersdgku <heatherchoe@gmail.com>
2024-06-14 08:47:47 -0700

    session3

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,49 @@
 function saveTask(){
     console.log("Saving tasks");
+    //get values from html
+    const title = $("#txtTitle").val();
+    const description = $("#txtDescription").val();
+    const color = $("#selColor").val();
+    const date = $("#selDate").val();
+    const status = $("#selStatus").val();
+    const budget = $("#numBudget").val();
+    //build an object
+    let taskToSave = new Task(title, description, color, date, status, budget);
+    console.log(taskToSave);
+    //save to server
+
+    //display the info from the server
+    displayTask(taskToSave);
+    //create a function that clears all inputs onclick save
+
 }
+
+function displayTask(task){
+
+    let syntax = `<div class='task'>
+    <div class='info'>
+        <h3>${task.title}</h3>
+        <h5${task.description}</h5>
+    </div>
+        <label class="status">${task.status}</label>
+        <div class="date-budget">
+            <label>${task.date}</label>
+            <label>${task.budget}</label>
+        </div>
+    </div>
+        `
+    $(".list-task").append(syntax);
+    //add the rest of the inputs into syntax
+
+}
+
 function init(){
     console.log("task manager");
 
     //load data
     
     //hook the events
-
+    
     $("#btnSave").click(saveTask);
 }
 
